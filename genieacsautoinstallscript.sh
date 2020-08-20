a0d38fd5db8fdd549c4e268a82b675947ffbe1ba
crh18 <cristiancrh18@hotmail.com>
2020-08-20 13:16:31 -0500

    Add files via upload
    v7

--- genieacsautoinstallscript.sh
+++ genieacsautoinstallscript.sh
@@ -1,31 +1,30 @@
-apt-get update
-apt-get upgrade
-#apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
-#cd /usr/src
+apt-get update 
+apt-get upgrade 
+#apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python libmysqlclient-dev mysql-server mysql-client
+#cd /usr/src 
 #wget http://prdownloads.sourceforge.net/webadmin/webmin_1.910_all.deb
-#dpkg --install webmin_1.910_all.deb
-apt-get install redis-server mongodb npm build-essential ruby-bundler ruby-dev libsqlite3-dev
-apt-get install mysql-server mysql-client default-libmysqlclient-dev nginx
-cd ~
+#dpkg --install webmin_1.910_all.deb 
+apt-get install redis-server mongodb npm build-essential ruby-bundler ruby-dev libsqlite3-dev libmysqlclient-dev lighttpd mysql-server mysql-client
+cd ~ 
 curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
 chmod +x nodesource_setup.sh
-./nodesource_setup.sh
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+./nodesource_setup.sh 
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - 
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list 
 apt-get update && apt-get install yarn
-apt-get install nodejs
-npm install libxmljs --unsafe-perm
-npm install -g genieacs --unsafe-perm
-git clone https://github.com/zaidka/genieacs-gui
+apt-get install nodejs 
+npm install libxmljs --unsafe-perm 
+npm install -g genieacs --unsafe-perm 
+git clone https://github.com/zaidka/genieacs-gui 
 apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev
-cd genieacs-gui
+cd genieacs-gui 
 bundle
-cp config/summary_parameters-sample.yml config/summary_parameters.yml
-cp config/index_parameters-sample.yml config/index_parameters.yml
-cp config/parameter_renderers-sample.yml config/parameter_renderers.yml
-cp config/parameters_edit-sample.yml config/parameters_edit.yml
-cp config/roles-sample.yml config/roles.yml
-cp config/users-sample.yml config/users.yml
+cp config/summary_parameters-sample.yml config/summary_parameters.yml 
+cp config/index_parameters-sample.yml config/index_parameters.yml 
+cp config/parameter_renderers-sample.yml config/parameter_renderers.yml 
+cp config/parameters_edit-sample.yml config/parameters_edit.yml 
+cp config/roles-sample.yml config/roles.yml 
+cp config/users-sample.yml config/users.yml 
 cp config/graphs-sample.json.erb config/graphs.json.erb
 cd db/migrate
 sudo sed -i '1!b;s/$/\[4.2]/g' *.rb
@@ -44,8 +43,8 @@ else
   tmux split-window
   tmux send-keys 'genieacs-fs' 'C-m'
   tmux split-window
-  tmux send-keys 'cd /root/genieacs-gui' 'C-m'
-  tmux send-keys 'sudo rails server -b 0.0.0.0' 'C-m'
+  tmux send-keys 'cd genieacs-gui' 'C-m'
+  tmux send-keys 'rails server -b 0.0.0.0' 'C-m'
   tmux select-layout tiled 2>/dev/null
   tmux rename-window 'GenieACS'
 
